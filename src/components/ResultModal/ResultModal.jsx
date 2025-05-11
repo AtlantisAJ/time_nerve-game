b8d2cc5df414d16d0be7644b0df4086fe63a1055
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-05-11 15:33:53 +0300

    Fix: Add ResultModal for TimerChallenge loss
    This commit introduces a ResultModal component to visually indicate when the timer challenge is lost, providing feedback to the user.

---
+++ src/components/ResultModal/ResultModal.jsx
@@ -0,0 +1,61 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+  const dialogRef = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialogRef.current.showModal();
+      }
+    };
+  });
+
+  return (
+    <dialog ref={dialogRef} className="result-modal">
+      <h2>You {result}</h2>
+      <p>The target time was <strong>{targetTime} seconds.</strong></p>
+      <p>You stopped the timer with <strong>X seconds left.</strong></p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
+
+// начиная с React до 19
+
+// export default function ResultModal({ref, result, targetTime}) {
+//   return (
+//     <dialog ref={ref} className="result-modal">
+//       <h2>You {result}</h2>
+//       <p>The target time was <strong>{targetTime} seconds.</strong></p>
+//       <p>The stopped the timer with <strong>X seconds left.</strong></p>
+//       <form method="dialog">
+//         <button>Close</button>
+//       </form>
+//     </dialog>
+//   );
+// }
+
+
+// прошлые версии React до 19
+
+// import React from 'react';
+//
+// const ResultModal = React.forwardRef(function ResultModal({result, targetTime}, ref) {
+//   return (
+//     <dialog ref={ref} className="result-modal">
+//       <h2>You {result}</h2>
+//       <p>The target time was <strong>{targetTime} seconds.</strong></p>
+//       <p>You stopped the timer with <strong>X seconds left.</strong></p>
+//       <form method="dialog">
+//         <button>Close</button>
+//       </form>
+//     </dialog>
+//   );
+// });
+//
+// export default ResultModal;
